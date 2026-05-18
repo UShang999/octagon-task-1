88615fba97ba4c04ed3bb83c9545aff6c2a49029
Shan <sviznoy216@gmail.com>
2026-05-18 11:15:59 +0300

    feat: add help, site and creator commands to bot

--- bot.js
+++ bot.js
@@ -1,15 +1,30 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-// Токен на бота
-const token = '8599667617:AAFhKdoFQxr2-xKP09BSgtD_3VQYJldsVqY';
-
-// Создаем экземпляр бота
+// Твой токен остается тем же
+const token = '8599667617:AAG3RfnUMkJPS-XJhYAK7_EqaADw0WQy_Oc';
 const bot = new TelegramBot(token, { polling: true });
 
-// Слушаем команду /start
+// Команда /start (оставим для приличия)
 bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Привет, октагон!');
+  bot.sendMessage(msg.chat.id, 'Привет, октагон!');
 });
 
-console.log('Бот запущен...');
+// 1. Команда /help - список команд
+bot.onText(/\/help/, (msg) => {
+  const helpText = `Список доступных команд:
+/site - Ссылка на сайт Октагона
+/creator - Информация об авторе бота`;
+  bot.sendMessage(msg.chat.id, helpText);
+});
+
+// 2. Команда /site - ссылка на сайт
+bot.onText(/\/site/, (msg) => {
+  bot.sendMessage(msg.chat.id, 'Сайт Октагона: https://octagon-edu.ru'); 
+});
+
+// 3. Команда /creator - твое ФИО
+bot.onText(/\/creator/, (msg) => {
+  bot.sendMessage(msg.chat.id, 'Автор бота: Гицел Полина'); 
+});
+
+console.log('Бот обновлен и запущен...');
